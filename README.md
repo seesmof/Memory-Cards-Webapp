64c17a92d2e0f67b3da7439c8848d46881decf6d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-14 12:39:20 +0300

    feat: setup project with Vite and React

--- README.md
+++
@@ -1,2 +0,0 @@
-# react memory cards game
- 
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="">Cards!</div>
+    </>
+  );
+}
+
+export default App;
